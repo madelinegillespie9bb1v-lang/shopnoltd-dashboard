4d9ee1a8ea3612c103574c8ce74dc61678a9edec
Shopno <shopno@example.com>
2026-03-12 22:55:44 +0600

    fix render port

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import os
 from config import Config
 from extensions import db, mail
 
@@ -7,36 +6,26 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize extensions
     db.init_app(app)
     mail.init_app(app)
 
-    # Import blueprints
     from routes.auth_routes import auth_bp
     from routes.dashboard_routes import dashboard_bp
     from routes.device_routes import device_bp
 
-    # Register blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(device_bp)
 
-    # Create database tables
     with app.app_context():
         from models.models import User, LoggedUser
         db.create_all()
 
-    # Health check
     @app.route("/ping")
     def ping():
-        return "ok", 200
+        return "ok"
 
     return app
 
 
 app = create_app()
-
-# ================= RUN =================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
